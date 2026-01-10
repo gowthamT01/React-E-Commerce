95afbf048967255678628aabf5303a50ee0902bb
Gowtham T <gtgowthu05@gmail.com>
2026-01-11 01:26:32 +0530

    Css Styles And Courses are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@
 import './App.css'
 import Navbar from './Navbar';
 
+
 import Footer from './Footer';
 import CourseList from './CourseList';
 
 function App() {
   return (
     <>
-      <Navbar/>
+    <Navbar/>
+  <video
+        className="bg-video"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src="/bg-video.mp4" type="video/mp4" />
+      </video>
+      
 
 <CourseList/>
       <Footer/>
